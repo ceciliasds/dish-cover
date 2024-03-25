2f5a8a8ea0fc7b775fb0d72b656c527e33087d28
ceciliasds <skudo540@gmail.com>
2024-03-25 21:08:55 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/dish_cover/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dish_cover
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/variables.dart
@@ -0,0 +1,6 @@
+class Variables {
+  static const String apiKey = '1';
+  static const String baseUrl = 'https://www.themealdb.com/api/json/v1/1';
+}
+
+List<Map<String, dynamic>> data = [];
